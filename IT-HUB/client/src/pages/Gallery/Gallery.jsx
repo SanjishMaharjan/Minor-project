687da8ce533a93a3dd1425d255788fa8473e9d74
Sanjish Maharjan <sanjishmaharjan619@gmail.com>
2023-02-24 00:42:47 +0545

    gallery

--- IT-HUB/client/src/pages/Gallery/Gallery.jsx
+++ IT-HUB/client/src/pages/Gallery/Gallery.jsx
@@ -65,7 +65,7 @@ const Gallery = () => {
     <>
       <div className="header">
         <h1 className="welcome">WELCOME TO THE GALLERY !!</h1>
-        <p className="welcome">FOR TOMORROW'S MEMORYS</p>
+        <p className="welcome">FOR TOMORROW'S MEMORIES</p>
       </div>
       <div className="wrapper">
         {imgCollection.map((img) => {
