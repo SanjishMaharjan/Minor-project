c53fb111cf3264522faa34f9f73e71966731577e
Rabi14378 <rabibro14378@gmail.com>
2023-02-28 16:09:18 +0545

    checking for valid mongoose id

---
+++ IT-HUB/server/middleWare/validateIdsMiddleware.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+function validateIds(...paramNames) {
+  return (req, res, next) => {
+    console.log("I am agian here");
+    console.log(req.params);
+    if (Object.keys(req.params).length !== 0) {
+      const invalidIds = [];
+      for (const paramName of paramNames) {
+        const id = req.params[paramName];
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+          invalidIds.push(paramName);
+        }
+      }
+      if (invalidIds.length > 0) {
+        const message = `Invalid ${invalidIds.join(", ")} ObjectId(s)`;
+        return res.status(400).json({ message });
+      }
+    }
+    return next();
+  };
+}
+
+module.exports = validateIds;
--- IT-HUB/server/routes/pollRoute.js
+++ IT-HUB/server/routes/pollRoute.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { protect } = require("../middleWare/authMiddleware");
+const validateIds = require("../middleWare/validateIdsMiddleware");
 const {
   registerCandidate,
   getPollForCandidacy,
@@ -8,10 +9,20 @@ const {
   deletePoll,
 } = require("../controllers/pollController");
 
-router.post("/:pollId/register", protect, registerCandidate);
-router.post("/:pollId/vote/:userId", protect, voteCandidate);
+router.post(
+  "/:pollId/register",
+  validateIds("pollId"),
+  protect,
+  registerCandidate
+);
+router.post(
+  "/:pollId/vote/:userId",
+  validateIds("pollId", "userId"),
+  protect,
+  voteCandidate
+);
 router.get("/register", getPollForCandidacy);
 router.get("/", getPollForVoting);
-router.delete("/:pollId", protect, deletePoll);
+router.delete("/:pollId", validateIds("pollId"), protect, deletePoll);
 
 module.exports = router;
