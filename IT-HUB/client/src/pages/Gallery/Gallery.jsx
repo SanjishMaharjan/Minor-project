4f08535ee36bef66304cceb605c499703e6f6870
Sanjish Maharjan <sanjishmaharjan619@gmail.com>
2023-03-07 23:35:29 +0545

    gallery updated

--- IT-HUB/client/src/pages/Gallery/Gallery.jsx
+++ IT-HUB/client/src/pages/Gallery/Gallery.jsx
@@ -11,10 +11,6 @@ const Gallery = () => {
     enabled: false,
   });
 
-  const openImg = (pic) => {
-    fullBox.style.display = "flex";
-    fullImg.src = pic;
-  };
 
   if (useNavigation().state === "loading") return <Loader />;
 
@@ -22,7 +18,7 @@ const Gallery = () => {
     <>
       <div className="header">
         <h1 className="welcome">WELCOME TO THE GALLERY !!</h1>
-        <p className="welcome">FOR TOMORROW'S MEMORIES</p>
+        {/* <p className="welcome">FOR TOMORROW'S MEMORIES</p> */}
       </div>
       <div className="gallery-wrapper">
         {imgCollection.map((img) => {
@@ -30,9 +26,17 @@ const Gallery = () => {
             <div key={img._id} className="gallery">
               <div className="gallery-item">
                 <img src={img.images[0].imagePath} onClick={() => setShowImageFullScreen(true)} />
-                <p className="gallery-detail">{img.title}</p>
-                <p className="gallery-detail">{img.description}</p>
-                <p className="gallery-detail">{getDate(img.createdAt)} ago</p>
+                <div className="gallery-detail">
+                  <h3>
+                    <lord-icon
+                      src="https://cdn.lordicon.com/ncxoarcp.json"
+                      trigger="hover"
+                    >
+                    </lord-icon> {img.title}</h3>
+                  <hr />
+                  <h4>{img.description}</h4>
+                  <p style={{ fontSize: "0.8rem" }}>{getDate(img.createdAt)} ago</p>
+                </div>
                 {showImageFullScreen && (
                   <div className="fullscreen-image" onClick={() => setShowImageFullScreen(false)}>
                     <img className="image" src={img.images[0].imagePath} alt="" />
